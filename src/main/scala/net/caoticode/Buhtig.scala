9c83a66682b505d023fcce67da1d3b02f6069fad
Daniel Camarda <maniacal.dread@gmail.com>
2014-05-28 15:06:17 +0200

    GHBuilder moved outside Buhtig, close method is now only in Buhtig class

--- src/main/scala/net/caoticode/Buhtig.scala
+++ src/main/scala/net/caoticode/Buhtig.scala
@@ -3,46 +3,43 @@ package net.caoticode
 import dispatch._, Defaults._
 
 class Buhtig(token: String) {
-  
+
   val authHeader = Map("Authorization" -> s"token $token")
   val github: Req = host("api.github.com").secure <:< authHeader
-  
-  def client = new GHBuilder(github)
-  
-  def close() = Http.shutdown()
-  
-  class GHBuilder(request: Req) extends Dynamic {
-    import scala.language.dynamics
-    
-    def selectDynamic(name: String) = new GHBuilder(request / name)
 
-    def applyDynamic(name: String)(args: Any*) = {
-      val req = args.foldLeft(request / name){ case (z, e) => z / e.toString}
-      
-      new GHBuilder(req)
-    }
-    
-    def ? (params: (String, String)*) = {
-      val req = params.foldLeft(request){ case (z, e) => z.addQueryParameter(e._1, e._2)}
-      
-      new GHBuilder(req)
-    }
-    
-    def get = Http(request OK as.String)
-    
-    def getOpt = {
-      val res = Http(request OK as.String).option
-      res()
-    }
-    
-    def getSync = {
-      val res = Http(request OK as.String)
-      res()
-    }
-    
-    def fullUrl(urlStr: String) = new GHBuilder(url(urlStr) <:< authHeader)
-    
-    def close() = Buhtig.this.close()
+  def client = new GHBuilder(github)
+
+  def close() = Http.shutdown()
+}
+
+class GHBuilder(request: Req) extends Dynamic {
+  import scala.language.dynamics
+
+  def selectDynamic(name: String) = new GHBuilder(request / name)
+
+  def applyDynamic(name: String)(args: Any*) = {
+    val req = args.foldLeft(request / name) { case (z, e) => z / e.toString }
+
+    new GHBuilder(req)
   }
-  
+
+  def ?(params: (String, String)*) = {
+    val req = params.foldLeft(request) { case (z, e) => z.addQueryParameter(e._1, e._2) }
+
+    new GHBuilder(req)
+  }
+
+  def get = Http(request OK as.String)
+
+  def getOpt = {
+    val res = Http(request OK as.String).option
+    res()
+  }
+
+  def getSync = {
+    val res = Http(request OK as.String)
+    res()
+  }
+
+  // def fullUrl(urlStr: String) = new GHBuilder(url(urlStr) <:< authHeader)
 }
